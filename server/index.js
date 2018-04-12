a76ff367656c42f64e7ae3a837c1ae9f49250268
seungprk@gmail.com <seungprk@gmail.com>
2018-04-12 12:21:33 -0700

    Fix linter issues

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
+
 const app = express();
 
 // Middleware
@@ -9,4 +10,4 @@ app.use(morgan('tiny'));
 // Static
 app.use('/', express.static(path.join(__dirname, '../client/dist')));
 
-app.listen(3001, () => console.log('server listening on port 3001...'));
+app.listen(3001);
