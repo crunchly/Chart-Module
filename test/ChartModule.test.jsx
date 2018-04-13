78d003f2fb17a6d3847de9108847330f698d74b9
seungprk@gmail.com <seungprk@gmail.com>
2018-04-13 11:20:14 -0700

    Add a basic enzyme test

--- test/ChartModule.test.jsx
+++ test/ChartModule.test.jsx
@@ -1,5 +1,20 @@
-const ChartModule = require('../client/src/components/ChartModule');
+import React from 'react';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import ChartModule from '../client/src/components/ChartModule';
 
-test('expects Chart component to exist', () => {
-  expect(ChartModule).toBeDefined();
+const { mount } = Enzyme;
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('<ChartModule />', () => {
+  test('expect Chart component to exist', () => {
+    expect(ChartModule).toBeDefined();
+  });
+
+  test('renders canvas', () => {
+    const wrapper = mount(<ChartModule />);
+    const result = wrapper.find('canvas').length;
+    expect(result).toBe(1);
+  });
 });
