5712f0731978b0f2a948f22d794099244c84facc
seungprk@gmail.com <seungprk@gmail.com>
2018-04-15 18:09:12 -0700

    Add ajax call to ChartModule

--- client/src/components/ChartModule.jsx
+++ client/src/components/ChartModule.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Chart from 'chart.js';
 
 class ChartModule extends React.Component {
@@ -8,47 +9,46 @@ class ChartModule extends React.Component {
   }
 
   componentDidMount() {
-    this.chart = new Chart(this.ref.current, {
-      type: 'bar',
-      data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-          label: '# of Votes',
-          data: [12, 19, 3, 5, 2, 3],
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)',
-          ],
-          borderColor: [
-            'rgba(255,99,132,1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)',
-          ],
-          borderWidth: 1,
-        }],
-      },
-      options: {
-        scales: {
-          yAxes: [{
-            ticks: {
-              beginAtZero: true,
+    this.fetchData()
+      .then((data) => {
+        this.chart = new Chart(this.ref.current, {
+          type: 'bar',
+          data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+              label: '# of Votes',
+              data: [12, 19, 3, 5, 2, 3],
+              backgroundColor: 'rgba(54, 162, 235, 0.2)',
+              borderColor: 'rgba(54, 162, 235, 1)',
+              borderWidth: 1,
+            }],
+          },
+          options: {
+            scales: {
+              yAxes: [{
+                ticks: {
+                  beginAtZero: true,
+                },
+              }],
             },
-          }],
-        },
-      },
-    });
+          },
+        });
+      });
+  }
+
+  fetchData() {
+    const url = `/funding-rounds/${this.props.company}`;
+    return fetch(url)
+      .then(response => response.json());
   }
 
   render() {
     return <canvas ref={this.ref} />;
   }
 }
+
+ChartModule.propTypes = {
+  company: PropTypes.string.isRequired,
+};
 
 export default ChartModule;
