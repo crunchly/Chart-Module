a74277ccb8de567d8a75bb436c6e66f9a91eb0d2
seungprk@gmail.com <seungprk@gmail.com>
2018-04-19 15:36:28 -0700

    Fix linter issues

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -6,9 +6,9 @@ const app = require('../server/app');
 
 test('get to "/" should get index.html', () => request(app).get('/').expect('Content-Type', /html/));
 
-test('get to "funding-rounds/:company" should get json', () => {
-  return request(app)
+test('get to "funding-rounds/:company" should get json', () => (
+  request(app)
     .get('/funding-rounds/Facebook')
     .set('Accept', 'application/json')
-    .then(response => expect(response.body[0].company).toBe('Facebook'));
-});
+    .then(response => expect(response.body[0].company).toBe('Facebook'))
+));
