8fc625f411d0cfb99579404564da9c9a99b584fa
seungprk@gmail.com <seungprk@gmail.com>
2018-04-25 16:21:25 -0700

    Have a fallback connection for localhost testing

--- db/db.js
+++ db/db.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://mongodb/front_end_capstone');
+mongoose.connect('mongodb://mongodb/front_end_capstone')
+  .catch(() => mongoose.connect('mongodb://localhost/front_end_capstone'))
 
 module.exports = mongoose.connection;
